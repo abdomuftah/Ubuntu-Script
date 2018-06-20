602d48a6d3f2b1f34f5d45870904a32cef833301
Abdallah Muftah <abdomuftah@hotmail.com>
2018-06-20 11:56:33 +0400

    Update ubuntu_script.sh

--- ubuntu_script.sh
+++ ubuntu_script.sh
@@ -10,7 +10,7 @@ echo "******************************************"
 echo ""
 #
 read -p 'Set Web Directory (Example: /var/www/html) ' directory
-read -p 'Set Web Domain (Example: example.example.com [Not trailing slash!]) ' domain
+read -p 'Set Web Domain (Example: 127.0.0.1 [Not trailing slash!]) ' domain
 #
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -20,13 +20,14 @@ sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:webupd8team/java -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo add-apt-repository -y ppa:chris-lea/redis-server
-sudo apt-get install screen
+sudo apt-get install screen -y
 sudo apt-get install nano
-sudo apt-get install curl
+sudo apt-get install curl -y
 sudo apt-get install git -y
 #
 sudo apt-get update
 sudo apt update -y
+sudo apt upgrade -y
 sudo apt-get install -y zip unzip
 #
 sudo apt install apache2 -y
@@ -50,9 +51,8 @@ sudo apt-get update
 sudo apt update
 sudo curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 #
-sudo apt-get install php7.2 
+sudo apt-get install php7.2 -y
 sudo apt install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip mariadb-server tar unzip redis-server sed composer 
-a2enmod php7.2
 systemctl restart apache2
 #
 echo "Install and Secure phpMyAdmin"
@@ -70,11 +70,6 @@ service apache2 restart
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
-#
-echo "Secureing with Let's Encrypt"
-sudo certbot --apache -d $domain
-#
-sudo certbot renew --dry-run
 #
 clear
 #
