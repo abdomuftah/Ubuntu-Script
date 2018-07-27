40413235f0bb9aa8012a2def08812b02b3aed4b3
Abdallah Muftah <abdomuftah@hotmail.com>
2018-07-27 11:44:54 +0400

    Update ubuntu_script.sh
    update to work with Ubuntu 18.04

--- ubuntu_script.sh
+++ ubuntu_script.sh
@@ -9,6 +9,7 @@ echo "*      LAMP server and phpMyAdmin        *"
 echo "******************************************"
 echo ""
 #
+read -p 'Root Password (past your user/root password ) ' rootpw
 read -p 'Set Web Directory (Example: /var/www/html) ' directory
 read -p 'Set Web Domain (Example: 127.0.0.1 [Not trailing slash!]) ' domain
 #
@@ -22,54 +23,39 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 sudo apt-get install screen -y
 sudo apt-get install nano
-sudo apt-get install curl -y
+sudo apt-get install curl 
 sudo apt-get install git -y
 #
 sudo apt-get update
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
 sudo apt-get install -y zip unzip
 #
 sudo apt install apache2 -y
-
+#
 sudo apt-get install ufw
-sudo apt-get install -y firewalld
-firewall-cmd --permanent --zone=public --add-service=http
-firewall-cmd --permanent --zone=public --add-service=https
-firewall-cmd --permanent --add-port=21/tcp
-firewall-cmd --permanent --add-port=40000/tcp
+sudo ufw app list
 sudo ufw app info "Apache Full"
 sudo ufw allow in "Apache Full"
-firewall-cmd --reload
 #
 sudo apt update
+sudo apt-get update
 sudo apt install mysql-server -y
 sudo mysql_secure_installation
 #
-sudo add-apt-repository ppa:ondrej/php -y
-sudo apt-get update
 sudo apt update
-sudo curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
 #
 sudo apt-get install php7.2 -y
-sudo apt install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip mariadb-server tar unzip redis-server sed composer 
-systemctl restart apache2
+sudo apt install php7.2 php7.2-cli php7.2-gd php7.2-mysql php7.2-pdo php7.2-mbstring php7.2-tokenizer php7.2-bcmath php7.2-xml php7.2-fpm php7.2-curl php7.2-zip tar redis-server sed composer 
+systemctl restart apache2 -$rootpw
 #
 echo "Install and Secure phpMyAdmin"
 sudo apt-get update
-sudo apt-get install phpmyadmin php7.2-mbstring php-gettext
+sudo apt-get install phpmyadmin php7.2-mbstring php-gettext -y
 sudo apt-get update
 #
-sed -i 's/Require ip 127.0.0.1/Require all granted/g' /etc/apache2/apache2.conf
-sed -i 's/Require ip ::1/#Require ip ::1/g' /etc/apache2/apache2.conf
-sed -i 's/Deny from All/Allow from All/g' /etc/apache2/apache2.conf
-sed -i 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
 a2enmod rewrite
-service apache2 restart
-#
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get install -y build-essential
+service apache2 restart -$rootpw
 #
 clear
 #
