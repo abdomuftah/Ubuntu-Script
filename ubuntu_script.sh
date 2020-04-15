1ea5903a7b3b4aad121b2c6d9d3c72ca5c36578e
Abdallah Muftah <abdomuftah@hotmail.com>
2020-04-15 17:10:32 +0400

    update to php7.4
    now you will install php7.4 , not 7.3 .
    And mariadb not mySQL

--- ubuntu_script.sh
+++ ubuntu_script.sh
@@ -12,7 +12,6 @@ echo "*      Domain with Let's Encrypt         *"
 echo "******************************************"
 echo ""
 #
-read -p 'Set Web Directory (Example: /var/www/html) ' directory
 read -p 'Set Web Domain (Example: 127.0.0.1 [Not trailing slash!]) ' domain
 #
 sudo apt update -y && sudo apt upgrade -y
@@ -23,11 +22,11 @@ sudo apt-get install software-properties-common -y
 sudo apt-get install python-software-properties -y
 sudo apt-add-repository ppa:webupd8team/java -y
 sudo add-apt-repository ppa:ondrej/php -y
+sudo add-apt-repository ppa:phpmyadmin/ppa -y
 sudo add-apt-repository ppa:certbot/certbot -y
-sudo add-apt-repository ppa:phpmyadmin/ppa
 sudo add-apt-repository -y ppa:chris-lea/redis-server
 sudo apt-get install -y screen nano curl git zip unzip ufw python-certbot-apache
-sudo apt-get install -y libmysqlclient-dev python-dev python-pip
+sudo apt-get install -y python3.7 libmysqlclient-dev python3-dev python3-pip
 #
 sudo apt update && sudo apt upgrade
 sudo apt-get update && sudo apt-get upgrade
@@ -48,12 +47,8 @@ sudo ufw allow in "Apache Full"
 echo "=================================="
 echo "installing mySQL :"
 echo "=================================="
-sudo apt-get install mysql-server
-# for OS 16.04
-sudo systemctl stop mysql.service
-sudo systemctl start mysql.service
-sudo systemctl enable mysql.service
-# for OS 18.04 and up
+sudo apt-get install mariadb-server mariadb-client
+#
 sudo systemctl stop mariadb.service
 sudo systemctl start mariadb.service
 sudo systemctl enable mariadb.service
@@ -68,7 +63,7 @@ sudo apt-get update && sudo apt-get upgrade
 echo "=================================="
 echo "installing PHP 7.3 + modules"
 echo "=================================="
-sudo apt install php7.3 libapache2-mod-php7.3 php7.3-common php7.3-mbstring php7.3-xmlrpc php7.3-soap php7.3-gd php7.3-xml php7.3-intl php7.3-mysql php7.3-cli php7.3-zip php7.3-curl php7.3-pdo php7.3-tokenizer php7.3-bcmath php7.3-fpm php7.3-imagick php7.3-recode php7.3-tidy tar redis-server sed composer
+sudo apt install php7.4 libapache2-mod-php7.4 php7.4-common php7.4-mbstring php7.4-xmlrpc php7.4-soap php7.4-gd php7.4-xml php7.4-intl php7.4-mysql php7.4-cli php7.4-zip php7.4-curl php7.4-pdo php7.4-tokenizer php7.4-bcmath php7.4-fpm php7.4-imagick php7.4-tidy tar redis-server sed composer
 sudo systemctl restart apache2.service
 #
 echo "=================================="
@@ -81,21 +76,20 @@ sudo apt-get install phpmyadmin php-gettext
 echo "=================================="
 echo "Update php.ini file "
 echo "=================================="
-wget https://raw.githubusercontent.com/abdomuftah/Ubuntu-Script/master/php.ini && mv -f php.ini /etc/php/7.3/apache2/
+wget https://raw.githubusercontent.com/abdomuftah/Ubuntu-Script/master/php.ini && mv -f php.ini /etc/php/7.4/apache2/
 #
 sudo a2enmod rewrite
 sudo systemctl restart apache2.service
 systemctl restart apache2
 #
 sudo apt update -y && sudo apt upgrade -y
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 #
 echo "=================================="
 echo "Installing nodeJS"
 echo "=================================="
 sudo curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt-get install -y nodejs
-sudo apt-get -y install gcc g++ make
+sudo apt-get -y install gcc g++ make nodejs
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -y && sudo apt-get install yarn
@@ -108,20 +102,21 @@ sudo certbot --apache -d $domain
 sudo certbot renew --dry-run
 #
 sudo apt update -y && sudo apt upgrade -y
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 sudo systemctl restart apache2.service
-#
-service mysql restart
 #
 clear
 #
 echo "your PHP Ver is :"
 php -v 
 #
-echo "your MSQL Ver is :"
-mysqladmin -u root -p version 
 echo ""
 echo "thanks for installing this script !!"
 echo "https://twitter.com/Scar_Naruto"
 #
+#
 exit
+
+
+
+
