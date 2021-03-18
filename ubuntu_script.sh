0ed8c61ab4195d169707bf259f8d8eada88d1bf5
Abdallah Muftah <scar.naruto@hotmail.com>
2021-03-19 03:29:54 +0400

    Update ubuntu_script.sh
    add bzip2 compression

--- ubuntu_script.sh
+++ ubuntu_script.sh
@@ -80,7 +80,7 @@ sudo apt-get upgrade -y
 echo "=================================="
 echo "installing PHP 7.4 + modules"
 echo "=================================="
-sudo apt install -y php7.4 libapache2-mod-php7.4 php7.4-common php7.4-mbstring php7.4-xmlrpc php7.4-soap php7.4-gd php7.4-xml php7.4-intl php7.4-mysql php7.4-cli php7.4-zip php7.4-curl php7.4-pdo php7.4-tokenizer php7.4-bcmath php7.4-fpm php7.4-imagick php7.4-tidy tar redis-server sed composer
+sudo apt install -y php7.4 libapache2-mod-php7.4 php7.4-common php7.4-mbstring php7.4-xmlrpc php7.4-soap php7.4-gd php7.4-bz2 php7.4-xml php7.4-intl php7.4-mysql php7.4-cli php7.4-zip php7.4-curl php7.4-pdo php7.4-tokenizer php7.4-bcmath php7.4-fpm php7.4-imagick php7.4-tidy tar redis-server sed composer
 sudo systemctl restart apache2.service
 #
 echo "=================================="
